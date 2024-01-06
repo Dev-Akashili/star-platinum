6438bafcf2a1035c5f7b635fffd3ddcb098eea35
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-01-06 01:41:16 +0000

    feat: add asp.net core backend
    - renamed client-app to frontend
    - added asp.net core backend
    - implements identity core endpoints for auth

---
+++ Backend/Program.cs
@@ -0,0 +1,51 @@
+using Backend.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure the EF Core context
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("Default");
+        if (string.IsNullOrWhiteSpace(connectionString))
+            options.UseNpgsql();
+        else
+            options.UseNpgsql(connectionString,
+                o => o.EnableRetryOnFailure());
+    });
+
+// Add Identity services to the container
+builder.Services.AddAuthorization();
+
+// Activate Identity APIs
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+// Map Identity routes
+app.MapIdentityApi<IdentityUser>();
+
+app.MapSwagger();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
