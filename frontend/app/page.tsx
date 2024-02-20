4d595fc4c9adfd42398258ff402228a6b684a9bf
Chukwuemeka Akashili <94780632+Dev-Akashili@users.noreply.github.com>
2024-02-20 16:23:29 +0000

    feat: update home page ui

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,34 @@
 import { Box, Flex, HStack } from "@chakra-ui/react";
 import Home from "./home/page";
 import { links } from "@/data";
-import { PageBtn } from "@/components/Button";
+import { PageBtn, PageIcon } from "@/components/Button";
 
 export default function Page() {
   return (
-    <Box>
-      <Flex
-        justifyContent={{ base: "center", lg: "right" }}
-        mt="25px"
-        mr={{ base: "0", lg: "10%" }}
-      >
-        <HStack>
+    <Box mt={{ base: "100px", lg: "120px" }}>
+      <Flex justifyContent="right" mt="25px" mr={{ base: "5%", lg: "10%" }}>
+        <HStack spacing={3}>
+          <PageIcon
+            link={links.org}
+            imageLink="https://img.icons8.com/material-outlined/48/github.png"
+            imageAlt="GitHub"
+          />
+          <PageIcon
+            link={links.org}
+            imageLink="https://img.icons8.com/material-sharp/48/linkedin--v1.png"
+            imageAlt="LinkedIn"
+          />
+          <PageIcon
+            link={links.org}
+            imageLink="https://img.icons8.com/material-outlined/48/twitterx--v2.png"
+            imageAlt="Twitter"
+          />
           <PageBtn
             name="Sponsor"
             link="/"
             target={false}
             imageLink="https://img.icons8.com/color/48/filled-like.png"
             imageAlt="heart"
-          />
-          <PageBtn
-            name="Contribute"
-            link={links.org}
-            target={true}
-            imageLink="https://img.icons8.com/fluency/48/star--v1.png"
-            imageAlt="star"
           />
         </HStack>
       </Flex>
--- frontend/data/index.ts
+++ frontend/data/index.ts
@@ -2,7 +2,7 @@ export const text = {
   home: {
     title: "Star Platinum Auth & User Management",
     description:
-      "An open source software product that streamlines user authentication, authorization, and management for developers. Built as a solution to the hassle of building custom auth systems, by providing intuitive APIendpoints and a user-friendly interface which simplifies integration and empowers you to focus on your core application.",
+      "An open source software product that streamlines user authentication, authorization, and management for developers.",
     cards: [
       {
         title: "Open Source",
@@ -39,6 +39,6 @@ export const text = {
 export const links = {
   github: "https://github.com/Dev-Akashili",
   org: "https://github.com/Star-Platinum-Auth/star-platinum",
-  linkedin:"https://www.linkedin.com/in/akashili/",
+  linkedin: "https://www.linkedin.com/in/akashili",
   mail: "https://mailto:chukwuemeka.akashili@gmail.com"
 };
